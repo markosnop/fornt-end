e71d08fea5ecdddf724eaa3f399ad4e3f3d67006
markosnop <markos.vpd@gmail.com>
2023-11-16 21:52:25 -0300

    first commit

---
+++ src/layout/Loading.js
@@ -0,0 +1,10 @@
+import "./Loading.css";
+function Loading() {
+  return (
+    <div className="loading-container">
+      <div className="loading-spinner"></div>
+    </div>
+  );
+}
+
+export default Loading;
